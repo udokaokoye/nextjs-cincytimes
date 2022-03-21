2a63ddec381b89316fe046d2cb79528ee6c7f630
Udoka Okoye <leviokoye@gmail.com>
2022-03-21 08:38:12 -0400

    s

--- pages/test.js
+++ pages/test.js
@@ -1,26 +1,16 @@
 import React from 'react'
-import { GetPostById } from '../lib/swr-hooks'
 
 
-const test = ({allPostsData}) => {
+const test = () => {
 
   return (
       <>
-      <title>{allPostsData[0].title}</title>
-    <div>{allPostsData[0].title}</div>
-    <a target={'_blank'} href="http://localhost:3000/test">test</a>
+      <h1>Heyy</h1>
       </>
   )
 }
 
-export async function getStaticProps() {
-    const allPostsData = await fetch('https://api.thecincinnatitimes.com/GET/get_post.php?post_id=623094dee2bb5').then((res) => res.json())
-    return {
-      props: {
-        allPostsData
-      }
-    }
-  }
+
 
 
 

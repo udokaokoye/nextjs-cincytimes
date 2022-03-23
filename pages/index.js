35fe9397ab287aa5979f8739ba88449040140a63
Udoka Okoye <leviokoye@gmail.com>
2022-03-23 09:28:09 -0400

    asd

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ const Home = () => {
     <Head>
       <meta charset="UTF-8" />
       <title>The Cincinnati Times</title>
-      <meta name="keywords" content="title, newspapper, cincinnati, cincinnatitimes, thecincinnatitimes.com, The Cincinnati Times, Cincinnati NewsPapper, Latest News, Top Newspappers" />
+      <meta name="keywords" content="title, newspaper, cincinnati, cincinnatitimes, thecincinnatitimes.com, The Cincinnati Times, Cincinnati NewsPaper, Latest News, Top Newspapers" />
       <meta name="descrition" content="The Cincinnati Times, News At Your Finger Tips" />
       {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
 
